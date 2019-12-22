250e070efdc636b4311f0c5a8e1adbab8be756f1
sysBlackFox <2650059376@qq.com>
2019-12-23 01:46:27 +0800

    data

--- frontPage/assets/js/scripts.js
+++ frontPage/assets/js/scripts.js
@@ -1,5 +1,8 @@
 
 jQuery(document).ready(function() {
+
+	var step= 1
+	var apiHost = 'http://127.0.0.1:8080'
 	
     /*
         Fullscreen background
@@ -9,6 +12,18 @@ jQuery(document).ready(function() {
     /*
         Form
     */
+
+   $('.date').datepicker({
+	format: 'yyyy-mm-dd',
+	language: 'zh-CN',
+	autoclose:true,
+	startView: 'years',
+	todayBtn:true,
+	todayHighlight:true,
+	clearBtn: true,
+	viewSelect:'day'
+   });
+
     $('.registration-form fieldset:first-child').fadeIn('slow');
     
     $('.registration-form input[type="text"], .registration-form input[type="password"], .registration-form textarea').on('focus', function() {
@@ -21,7 +36,7 @@ jQuery(document).ready(function() {
     	var next_step = true;
     	
     	parent_fieldset.find('input[type="text"], input[type="password"], textarea').each(function() {
-    		if( $(this).val() == "" ) {
+			if( $(this).attr('required') && $(this).val() == "" ) {
     			$(this).addClass('input-error');
     			next_step = false;
     		}
@@ -31,6 +46,7 @@ jQuery(document).ready(function() {
     	});
     	
     	if( next_step ) {
+			step++
     		parent_fieldset.fadeOut(400, function() {
 	    		$(this).next().fadeIn();
 	    	});
@@ -40,6 +56,7 @@ jQuery(document).ready(function() {
     
     // previous step
     $('.registration-form .btn-previous').on('click', function() {
+		step--
     	$(this).parents('fieldset').fadeOut(400, function() {
     		$(this).prev().fadeIn();
     	});
@@ -47,18 +64,144 @@ jQuery(document).ready(function() {
     
     // submit
     $('.registration-form').on('submit', function(e) {
-    	
+		e.preventDefault();
+		var next_step = true;
+		
     	$(this).find('input[type="text"], input[type="password"], textarea').each(function() {
-    		if( $(this).val() == "" ) {
-    			e.preventDefault();
-    			$(this).addClass('input-error');
+    		if( $(this).attr('required') && $(this).val() == "" ) {
+				$(this).addClass('input-error');
+				next_step = false;
     		}
     		else {
     			$(this).removeClass('input-error');
     		}
-    	});
+		});
+		
+		if( next_step ) {			
+			let jsonData = $(".registration-form").serializeObject()
+			if(jsonData.birthday){
+				jsonData.birthday = new Date(jsonData.birthday).getTime()/1000
+			}
+			if(jsonData.graduationTime){
+				jsonData.graduationTime = new Date(jsonData.graduationTime).getTime()/1000
+			}
+    		$.ajax({
+				type:'post',
+				url: apiHost +'/front/user/register',
+				contentType:'application/json',
+				data:JSON.stringify({
+					body:jsonData,
+					header:{}
+				}),
+				success:function(res){
+					if(res.header && res.header.code == "SUCCESS"){
+						swal({
+							title: "提交成功!",
+							text: "恭喜您，感谢您的信任!",
+							icon: "success",
+							button: "确定",
+						  });
+					}else{
+						swal({
+							title: "提交失败!",
+							text: res.header.msg,
+							icon: "error",
+							button: "确定",
+						  });
+					}
+				}
+			})
+    	}
     	
-    });
-    
-    
+	});
+	initSerializeObject();
+	getAdmin()
+	getcourse()
+	geteducation()
+
+	function initSerializeObject() {
+		$.fn.serializeObject = function () {
+			var o = {};
+			var a = this.serializeArray();
+			$.each(a, function () {
+				if (o[this.name]) {
+					if (!o[this.name].push) {
+						o[this.name] = [o[this.name]];
+					}
+					o[this.name].push(this.value || '');
+				} else {
+					o[this.name] = this.value || '';
+				}
+			});
+			return o;
+		};
+	}
+	
+	function getAdmin(){
+		$.ajax({
+			type:'post',
+			url: apiHost + '/back/admin/search',
+			contentType:'application/json',
+			data:JSON.stringify({
+				body:{
+					roleId:4
+				},
+				header:{}
+			}),
+			success:function(res){
+				if(res.header && res.header.code == "SUCCESS"){
+					if(res.body && res.body.length){
+						var html = res.body.map(function(item){
+							return '<option value="'+item.adminId+'">'+item.nickname+'</option>'
+						}).join('')
+						$('#adminId').html(html)
+					}
+				}
+			}
+		})
+	}
+
+	function getcourse(){
+		$.ajax({
+			type:'post',
+			url:apiHost +'/back/course/all',
+			contentType:'application/json',
+			data:JSON.stringify({
+				body:{},
+				header:{}
+			}),
+			success:function(res){
+				if(res.header && res.header.code == "SUCCESS"){
+					if(res.body && res.body.length){
+						var html = res.body.map(function(item){
+							return '<option value="'+item.courseId +'">'+item.courseName +'</option>'
+						}).join('')
+						$('#courseId').html(html)
+					}
+				}
+			}
+		})
+	}
+
+	function geteducation(){
+		$.ajax({
+			type:'post',
+			url:apiHost +'/back/user/education',
+			contentType:'application/json',
+			data:JSON.stringify({
+				body:{},
+				header:{}
+			}),
+			success:function(res){
+				if(res.header && res.header.code == "SUCCESS"){
+					if(res.body && res.body.length){
+						var html = res.body.map(function(item){
+							return '<option value="'+item.educationId +'">'+item.educationName +'</option>'
+						}).join('')
+						$('#educationId').html(html)
+					}
+				}
+			}
+		})
+	}  
 });
